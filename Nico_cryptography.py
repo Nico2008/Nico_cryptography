4432e7ee61d59df8e32179b90ea93b6daad404f7
granders19 <geoffanders@gmail.com>
2018-11-14 16:34:36 -0800

    made it so that encoding will skip any characters that are not letters

--- Nico_cryptography.py
+++ Nico_cryptography.py
@@ -2,22 +2,17 @@
 alpha = "abcdefghijklmnopqrstuvwxwz"
 alpha = list(alpha)
 
-message = "Hello Nico".lower()
-
-
-offset = 6
-
 def encode(msg, off):
     output_message = ""
-    for char in message:
-        if (char==' '):
-            output_message += ' '
+    msg = msg.lower()
+    for char in msg:
+        if char not in alpha:
+            output_message += char 
             continue
         ind = alpha.index(char)
         output_message += alpha[(ind + off)%26]
     return output_message
 
-print(encode("It is almost xmas",26))
 
 
 
